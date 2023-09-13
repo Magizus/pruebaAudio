132d078a50942bcfec917cc9eab45f8505ae840e
Jose Andres <joseasalazar04@gmail.com>
2023-09-13 11:04:31 -0600

    subida archivos

---
+++ src/main/java/musica/Musica.java
@@ -0,0 +1,12 @@
+
+
+package musica;
+
+
+public class Musica {
+
+    public static void main(String[] args) {
+        audio reproducir = new audio();
+        reproducir.setVisible(true);
+    }
+}
